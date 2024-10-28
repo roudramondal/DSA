67c136eff8c1b9d6593e6bf78e7f624d4ce90dce
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-10-28 20:52:09 +0600

    Add files via upload

---
+++ Basic2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  double a=100000;
+  double b=100000;
+  double c= a*b;
+  cout<<fixed<<c<<endl;//if we do not use fixed then output is 1e+010;
+  return 0;
+}
---
+++ Basic3.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  // simple program
+  // int n;
+  // cin>>n;
+  // for(int i=1;i<=n;i++)
+  // {
+  //   for(int j=1;j<=i;j++)
+  //   {
+  //    cout<<"*";
+  //   }
+  //   cout<<endl;
+  //   }
+  // now t test cases 
+  // int t;
+  // cin>>t;
+  // for(int test =0;test<=t;++test)
+  // {
+  //   int a;
+  //   cin>>a;
+  //   for(int i =1;i<=a;++i)
+  //   {
+  //     for(int j=1;j<=i;++j)
+  //     {
+  //       cout<<"*";
+  //     }
+  //     cout<<endl;
+  //   }
+  // }
+  cout<<5;
+    
+    return 0;
+}
