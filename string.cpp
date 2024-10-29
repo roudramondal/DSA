cfe77a6919fa2a6efb0e6b486b861b2193a52634
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-10-29 23:30:03 +0600

    Add files via upload

---
+++ string.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  string st1 = "Hello";
+  string st2 = "World";
+  string result = st1 + st2;
+  cout << " " << result << endl;
+  return 0;
+}
