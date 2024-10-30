91dd2943d13ff8fffc6900b7f18b4f0cc79e78c9
Roudra <103403881+roudramondal@users.noreply.github.com>
2024-10-30 23:36:04 +0600

    Add files via upload

---
+++ string3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+  int t;
+  cin >> t;
+  cin.ignore();// cin.ignore usr for remove next line 
+  while (t--)
+  {
+    string s;
+    getline(cin, s);
+    cout << s << endl;
+  }
+  return 0;
+}
